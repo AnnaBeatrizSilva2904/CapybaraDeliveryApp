4a0df81ea4bbd8ea5530830ae19d85b4f6d7009a
AnnaBeatrizSilva2904 <annabeatryz2504@gmail.com>
2025-08-25 10:47:57 -0300

    Estilizando o projeto

---
+++ capybaraDelivery/app/_layout.tsx
@@ -0,0 +1,10 @@
+import CustomHeader from "@/components/CustomHeader";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ header: () => <CustomHeader/> }}/>
+    </Stack>
+  );
+}
